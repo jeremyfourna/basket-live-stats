c9d252937f94d54d8e59b4b1bee8e3eecb899c8a
dexterneo <jeremyfourna@gmail.com>
2015-03-29 21:01:22 +0200

    big update of the project

--- client/templates/myGames/ongoingGames.js
+++ client/templates/myGames/ongoingGames.js
@@ -1,5 +1,14 @@
-Template.newGame.events({
-	'click button': function() {
+Template.ongoingGames.helpers({
+	runningGames: function() {
+		return Games.find({
+			state: 'running',
+			userId: Meteor.userId()
+		});
+	}
+});
+
+Template.ongoingGames.events({
+	'click .newGame': function() {
 		Meteor.call('gameInsert', function(error, result) {
 			// display the error to the user and abort
 			if (error)
