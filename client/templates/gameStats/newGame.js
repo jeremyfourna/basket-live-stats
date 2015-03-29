e81bc78ef9fe8633d57b6c61dfac30c8e42bbfa5
dexterneo <jeremyfourna@gmail.com>
2015-03-29 22:29:57 +0200

    big update of game creation process

--- client/templates/gameStats/newGame.js
+++ client/templates/gameStats/newGame.js
@@ -1,12 +1,42 @@
+Session.setDefault('step2Visited', false);
+Session.setDefault('step3Visited', false);
+
+Template.newGame.helpers({
+	hasIVisitedStep2: function() {
+		return Session.get('step2Visited');
+	},
+	hasIVisitedStep3: function() {
+		return Session.get('step3Visited');
+	}
+});
+
 Template.newGame.events({
-	'click .newGame': function() {
-		Meteor.call('gameInsert', function(error, result) {
-			// display the error to the user and abort
-			if (error)
-				return throwError(error.reason);
-			Router.go('gameStats', {
-				_id: result._id
-			});
-		});
+	'click #step1FFBB': function() {
+		$('#step1').removeClass('show').addClass('hidden');
+		$('#step2').removeClass('hidden').addClass('show').addClass('visited');
+		Session.set('step2Visited', true);
+	},
+	'click #step1Amical': function() {
+		$('#step1').removeClass('show').addClass('hidden');
+		$('#step2').removeClass('hidden').addClass('show').addClass('visited');
+		Session.set('step2Visited', true);
+	},
+	'click #step2FFBB': function() {
+		$('#step2').removeClass('show').addClass('hidden');
+		$('#step3').removeClass('hidden').addClass('show').addClass('visited');
+		Session.set('step3Visited', true);
+	},
+	'click #step2Amical': function() {
+		$('#step2').removeClass('show').addClass('hidden');
+		$('#step3').removeClass('hidden').addClass('show').addClass('visited');
+		Session.set('step3Visited', true);
+	},
+	'click #step2previous': function() {
+		$('#step2').removeClass('show').addClass('hidden');
+		$('#step1').removeClass('hidden').addClass('show');
+	},
+	'click #step3previous': function() {
+		$('#step3').removeClass('show').addClass('hidden');
+		$('#step2').removeClass('hidden').addClass('show');
 	}
 });
--- client/templates/myGames/ongoingGames.js
+++ client/templates/myGames/ongoingGames.js
@@ -6,16 +6,3 @@ Template.ongoingGames.helpers({
 		});
 	}
 });
-
-Template.ongoingGames.events({
-	'click .newGame': function() {
-		Meteor.call('gameInsert', function(error, result) {
-			// display the error to the user and abort
-			if (error)
-				return throwError(error.reason);
-			Router.go('gameStats', {
-				_id: result._id
-			});
-		});
-	}
-});
