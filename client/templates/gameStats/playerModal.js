0a349f57c8b6499103fe3120da6eee0eedc892b3
dexterneo <jeremyfourna@gmail.com>
2015-04-09 14:00:35 +0200

    Currently adding FT stats (fail)

--- client/templates/gameStats/playerModal.js
+++ client/templates/gameStats/playerModal.js
@@ -3,3 +3,26 @@ Template.playerModal.helpers({
 		return Session.get('currentPlayerForModal').firstName.charAt(0) + ". " + Session.get('currentPlayerForModal').lastName;
 	}
 });
+
+Template.playerModal.events({
+	'click #onePoint': function(e) {
+		var index = Session.get('currentPlayerForModal').index;
+		var getTeam = function() {
+			if (Session.get('currentPlayerForModal').team === 'homeTeam') {
+				return this.gameInfos.homeTeam;
+			} else {
+				return this.gameInfos.awayTeam;
+			}
+		};
+		var team = getTeam();
+		var ftShoot = this.team.players['index'].stats.off.ftShoot;
+		var ftIn = this.team.players['index'].stats.off.ftIn;
+		Games.update(this._id, {
+			$inc: {
+				ftShoot: 1,
+				ftIn: 1
+			}
+		});
+		return console.log("1 point for number" + Session.get('currentPlayerForModal').jersey + " of " + team);
+	}
+});
--- client/templates/gameStats/playerPill.js
+++ client/templates/gameStats/playerPill.js
@@ -1,27 +1,62 @@
 Session.setDefault('currentPlayerForModal', null);
 
-Template.playerPill.events({
-	'click .pillForModal': function(e) {
+Template.homeTeamPlayerPill.events({
+	'click .homeTeamPillForModal': function(e) {
 		if (this.firstName === null && this.lastName === null) {
 			return Session.set('currentPlayerForModal', {
 				firstName: "",
 				lastName: "",
 				jersey: this.jersey,
-				playerIndex: this.playerIndex
+				playerIndex: this.playerIndex,
+				team: "homeTeam"
 			});
 		} else if (this.firstName === null) {
 			return Session.set('currentPlayerForModal', {
 				firstName: "",
 				lastName: this.lastName,
 				jersey: this.jersey,
-				playerIndex: this.playerIndex
+				playerIndex: this.playerIndex,
+				team: "homeTeam"
 			});
 		} else if (this.lastName === null) {
 			return Session.set('currentPlayerForModal', {
 				firstName: this.firstName,
 				lastName: "",
 				jersey: this.jersey,
-				playerIndex: this.playerIndex
+				playerIndex: this.playerIndex,
+				team: "homeTeam"
+			});
+		} else {
+			return Session.set('currentPlayerForModal', this);
+		}
+	}
+});
+
+Template.awayTeamPlayerPill.events({
+	'click .awayTeamPillForModal': function(e) {
+		if (this.firstName === null && this.lastName === null) {
+			return Session.set('currentPlayerForModal', {
+				firstName: "",
+				lastName: "",
+				jersey: this.jersey,
+				playerIndex: this.playerIndex,
+				team: "awayTeam"
+			});
+		} else if (this.firstName === null) {
+			return Session.set('currentPlayerForModal', {
+				firstName: "",
+				lastName: this.lastName,
+				jersey: this.jersey,
+				playerIndex: this.playerIndex,
+				team: "awayTeam"
+			});
+		} else if (this.lastName === null) {
+			return Session.set('currentPlayerForModal', {
+				firstName: this.firstName,
+				lastName: "",
+				jersey: this.jersey,
+				playerIndex: this.playerIndex,
+				team: "awayTeam"
 			});
 		} else {
 			return Session.set('currentPlayerForModal', this);
