9d1ac6e8a5cba08e1134dc6a9fd7d70291e99a84
dexterneo <jeremyfourna@gmail.com>
2015-04-12 12:44:26 +0200

    Add offensive rebound stat

--- client/templates/gameStats/playerModal.js
+++ client/templates/gameStats/playerModal.js
@@ -1771,5 +1771,246 @@ Template.playerModal.events({
 			}
 		}
 		return console.log("Number " + Session.get('currentPlayerForModal').jersey + " of " + Session.get('currentPlayerForModal').team + " just gived an assist !");
+	},
+	'click #offReb': function(e) {
+		var team = Session.get('currentPlayerForModal').team;
+		var playerIndex = Session.get('currentPlayerForModal').playerIndex;
+		var parentData = Template.parentData(1);
+		if (Session.get('currentPlayerForModal').playerIndex === 0) {
+			if (Session.get('currentPlayerForModal').team === "homeTeam") {
+				Games.update({
+					_id: parentData._id
+				}, {
+					$inc: {
+						"homeTeam.players.0.stats.rebonds.offReb": 1
+					}
+				});
+			} else {
+				Games.update({
+					_id: parentData._id
+				}, {
+					$inc: {
+						"awayTeam.players.0.stats.rebonds.offReb": 1
+					}
+				});
+			}
+		} else if (Session.get('currentPlayerForModal').playerIndex === 1) {
+			if (Session.get('currentPlayerForModal').team === "homeTeam") {
+				Games.update({
+					_id: parentData._id
+				}, {
+					$inc: {
+						"homeTeam.players.1.stats.rebonds.offReb": 1
+					}
+				});
+			} else {
+				Games.update({
+					_id: parentData._id
+				}, {
+					$inc: {
+						"awayTeam.players.1.stats.rebonds.offReb": 1
+					}
+				});
+			}
+		} else if (Session.get('currentPlayerForModal').playerIndex === 2) {
+			if (Session.get('currentPlayerForModal').team === "homeTeam") {
+				Games.update({
+					_id: parentData._id
+				}, {
+					$inc: {
+						"homeTeam.players.2.stats.rebonds.offReb": 1
+					}
+				});
+			} else {
+				Games.update({
+					_id: parentData._id
+				}, {
+					$inc: {
+						"awayTeam.players.2.stats.rebonds.offReb": 1
+					}
+				});
+			}
+		} else if (Session.get('currentPlayerForModal').playerIndex === 3) {
+			if (Session.get('currentPlayerForModal').team === "homeTeam") {
+				Games.update({
+					_id: parentData._id
+				}, {
+					$inc: {
+						"homeTeam.players.3.stats.rebonds.offReb": 1
+					}
+				});
+			} else {
+				Games.update({
+					_id: parentData._id
+				}, {
+					$inc: {
+						"awayTeam.players.3.stats.rebonds.offReb": 1
+					}
+				});
+			}
+		} else if (Session.get('currentPlayerForModal').playerIndex === 4) {
+			if (Session.get('currentPlayerForModal').team === "homeTeam") {
+				Games.update({
+					_id: parentData._id
+				}, {
+					$inc: {
+						"homeTeam.players.4.stats.rebonds.offReb": 1
+					}
+				});
+			} else {
+				Games.update({
+					_id: parentData._id
+				}, {
+					$inc: {
+						"awayTeam.players.4.stats.rebonds.offReb": 1
+					}
+				});
+			}
+		} else if (Session.get('currentPlayerForModal').playerIndex === 5) {
+			if (Session.get('currentPlayerForModal').team === "homeTeam") {
+				Games.update({
+					_id: parentData._id
+				}, {
+					$inc: {
+						"homeTeam.players.5.stats.rebonds.offReb": 1
+					}
+				});
+			} else {
+				Games.update({
+					_id: parentData._id
+				}, {
+					$inc: {
+						"awayTeam.players.5.stats.rebonds.offReb": 1
+					}
+				});
+			}
+		} else if (Session.get('currentPlayerForModal').playerIndex === 6) {
+			if (Session.get('currentPlayerForModal').team === "homeTeam") {
+				Games.update({
+					_id: parentData._id
+				}, {
+					$inc: {
+						"homeTeam.players.6.stats.rebonds.offReb": 1
+					}
+				});
+			} else {
+				Games.update({
+					_id: parentData._id
+				}, {
+					$inc: {
+						"awayTeam.players.6.stats.rebonds.offReb": 1
+					}
+				});
+			}
+		} else if (Session.get('currentPlayerForModal').playerIndex === 7) {
+			if (Session.get('currentPlayerForModal').team === "homeTeam") {
+				Games.update({
+					_id: parentData._id
+				}, {
+					$inc: {
+						"homeTeam.players.7.stats.rebonds.offReb": 1
+					}
+				});
+			} else {
+				Games.update({
+					_id: parentData._id
+				}, {
+					$inc: {
+						"awayTeam.players.7.stats.rebonds.offReb": 1
+					}
+				});
+			}
+		} else if (Session.get('currentPlayerForModal').playerIndex === 8) {
+			if (Session.get('currentPlayerForModal').team === "homeTeam") {
+				Games.update({
+					_id: parentData._id
+				}, {
+					$inc: {
+						"homeTeam.players.8.stats.rebonds.offReb": 1
+					}
+				});
+			} else {
+				Games.update({
+					_id: parentData._id
+				}, {
+					$inc: {
+						"awayTeam.players.8.stats.rebonds.offReb": 1
+					}
+				});
+			}
+		} else if (Session.get('currentPlayerForModal').playerIndex === 9) {
+			if (Session.get('currentPlayerForModal').team === "homeTeam") {
+				Games.update({
+					_id: parentData._id
+				}, {
+					$inc: {
+						"homeTeam.players.9.stats.rebonds.offReb": 1
+					}
+				});
+			} else {
+				Games.update({
+					_id: parentData._id
+				}, {
+					$inc: {
+						"awayTeam.players.9.stats.rebonds.offReb": 1
+					}
+				});
+			}
+		} else if (Session.get('currentPlayerForModal').playerIndex === 10) {
+			if (Session.get('currentPlayerForModal').team === "homeTeam") {
+				Games.update({
+					_id: parentData._id
+				}, {
+					$inc: {
+						"homeTeam.players.10.stats.rebonds.offReb": 1
+					}
+				});
+			} else {
+				Games.update({
+					_id: parentData._id
+				}, {
+					$inc: {
+						"awayTeam.players.10.stats.rebonds.offReb": 1
+					}
+				});
+			}
+		} else if (Session.get('currentPlayerForModal').playerIndex === 11) {
+			if (Session.get('currentPlayerForModal').team === "homeTeam") {
+				Games.update({
+					_id: parentData._id
+				}, {
+					$inc: {
+						"homeTeam.players.11.stats.rebonds.offReb": 1
+					}
+				});
+			} else {
+				Games.update({
+					_id: parentData._id
+				}, {
+					$inc: {
+						"awayTeam.players.11.stats.rebonds.offReb": 1
+					}
+				});
+			}
+		} else if (Session.get('currentPlayerForModal').playerIndex === 12) {
+			if (Session.get('currentPlayerForModal').team === "homeTeam") {
+				Games.update({
+					_id: parentData._id
+				}, {
+					$inc: {
+						"homeTeam.players.12.stats.rebonds.offReb": 1
+					}
+				});
+			} else {
+				Games.update({
+					_id: parentData._id
+				}, {
+					$inc: {
+						"awayTeam.players.12.stats.rebonds.offReb": 1
+					}
+				});
+			}
+		}
+		return console.log("Number " + Session.get('currentPlayerForModal').jersey + " of " + Session.get('currentPlayerForModal').team + " just catched an offensive rebond !");
 	}
 });
