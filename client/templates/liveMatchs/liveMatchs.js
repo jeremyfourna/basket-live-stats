a4479f7b97998337b00c8e11f9b8a3721fad877e
dexterneo <jeremyfourna@gmail.com>
2015-04-19 09:23:33 +0200

    Update the display of the live matchs

--- client/templates/liveMatchs/liveMatchs.js
+++ client/templates/liveMatchs/liveMatchs.js
@@ -11,5 +11,61 @@ Template.liveMatchs.helpers({
 				'state': 1
 			}
 		});
+	},
+	'notStarted': function() {
+		if (this.state === 'notStarted') {
+			return true;
+		} else {
+			return false;
+		}
+	},
+	'q1Running': function() {
+		if (this.state === 'q1Running') {
+			return true;
+		} else {
+			return false;
+		}
+	},
+	'q1Ended': function() {
+		if (this.state === 'q1Ended') {
+			return true;
+		} else {
+			return false;
+		}
+	},
+	'q2Running': function() {
+		if (this.state === 'q2Running') {
+			return true;
+		} else {
+			return false;
+		}
+	},
+	'halfTime': function() {
+		if (this.state === 'halfTime') {
+			return true;
+		} else {
+			return false;
+		}
+	},
+	'q3Running': function() {
+		if (this.state === 'q3Running') {
+			return true;
+		} else {
+			return false;
+		}
+	},
+	'q3Ended': function() {
+		if (this.state === 'q3Ended') {
+			return true;
+		} else {
+			return false;
+		}
+	},
+	'q4Running': function() {
+		if (this.state === 'q4Running') {
+			return true;
+		} else {
+			return false;
+		}
 	}
 });
