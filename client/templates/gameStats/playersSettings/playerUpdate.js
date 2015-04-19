16b61358a3cfabb1ecd7b527897c3a28b1f437e5
dexterneo <jeremyfourna@gmail.com>
2015-04-19 20:22:53 +0200

    Update the playersSettings

--- client/templates/gameStats/playersSettings/playerUpdate.js
+++ client/templates/gameStats/playersSettings/playerUpdate.js
@@ -15,13 +15,24 @@ Template.playerUpdate.events({
 		$(updateSelector).closest('.player').find('.lastName').removeAttr('disabled');
 		$(updateSelector).closest('.player').find(validateSelector).removeClass('disabled');
 	},
-	'click .playerValidation': function() {
+	'click .playerValidation': function(e) {
 		var updateSelector = '#' + this.team + this.jersey + 'update';
 		var validateSelector = '#' + this.team + this.jersey + 'validation';
+		var gameId = Template.parentData(1)._id;
+		var playerInfo = {
+			firstName: $(validateSelector).closest('.player').find('.firstName').val(),
+			lastName: $(validateSelector).closest('.player').find('.lastName').val(),
+			team: this.team,
+			playerIndex: this.playerIndex,
+			gameId: gameId
+		};
 		$(validateSelector).closest('.player').find('.firstName').attr('disabled', 'disabled');
 		$(validateSelector).closest('.player').find('.lastName').attr('disabled', 'disabled');
 		$(validateSelector).addClass('disabled');
-		var gameId = Template.parentData(1)._id
-		Meteor.call('playerUpdate', gameId, this, function(error, result) {});
+		Meteor.call('playerUpdate', playerInfo, function(error) {
+			if (error) {
+				return throwError(error.message);
+			}
+		});
 	}
 });
