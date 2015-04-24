dabdbaf53d33ab883104a9faf940dc3619eacf88
dexterneo <jeremyfourna@gmail.com>
2015-04-24 13:44:21 +0200

    Add update on the userName and allow delete of an account

--- client/templates/application/settings/settings.js
+++ client/templates/application/settings/settings.js
@@ -8,3 +8,33 @@ Template.settings.helpers({
 		});
 	}
 });
+
+Template.settings.events({
+	"click #loginInfosPseudoValidate": function() {
+		var user = {
+			pseudo: $('#pseudo').val(),
+			id: Meteor.userId()
+		};
+		Meteor.call('changeUserName', user, function(error) {
+			if (error) {
+				return throwError(error.message);
+			} else {
+				Meteor.logout();
+				Router.go('home');
+			}
+		});
+	},
+	"click #deleteAccount": function() {
+		var userId = {
+			_id: Meteor.userId()
+		};
+		Meteor.call('deleteUser', userId, function(error) {
+			if (error) {
+				return throwError(error.message);
+			} else {
+				Meteor.logout();
+				Router.go('home');
+			}
+		});
+	}
+});
