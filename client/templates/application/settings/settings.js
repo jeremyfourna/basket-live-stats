8f9d3a0567b9ee6d199c24225d8f76ae351ce036
dexterneo <jeremyfourna@gmail.com>
2015-04-24 14:02:04 +0200

    Add update of name, fistname and email

--- client/templates/application/settings/settings.js
+++ client/templates/application/settings/settings.js
@@ -36,5 +36,24 @@ Template.settings.events({
 				Router.go('home');
 			}
 		});
+	},
+	"click #personalInfosValidate": function() {
+		var validateEmail = function(mail) {
+			if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)) {
+				return mail;
+			}
+			return throwError("Please enter a valid email address.");
+		};
+		var user = {
+			id: Meteor.userId(),
+			name: $('#name').val(),
+			firstName: $('#firstName').val(),
+			email: validateEmail($('#email').val())
+		};
+		Meteor.call('updateUserProfile', user, function(error) {
+			if (error) {
+				return throwError(error.message);
+			}
+		});
 	}
 });
