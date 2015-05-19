cba75c43b13b712ca6eb9cae19519e18e3d14fe4
dexterneo <jeremyfourna@gmail.com>
2015-05-19 13:21:42 +0200

    Remove obsolete code

--- client/templates/application/header.js
+++ client/templates/application/header.js
@@ -1,11 +1,4 @@
 Template.header.helpers({
-	LBBSAdmin: function() {
-		if (Meteor.user().profile.LBBSAdmin) {
-			return true;
-		} else {
-			return false;
-		}
-	},
 	isClubAdmin: function() {
 		if (Meteor.user().profile.clubAdmin) {
 			return true;
