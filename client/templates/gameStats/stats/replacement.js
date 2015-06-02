25f6490b10771bfd9504234674cd45b1ed0f7fa1
dexterneo <jeremyfourna@gmail.com>
2015-06-02 13:52:32 +0200

    Call goingInPlay on drop event

--- client/templates/gameStats/stats/replacement.js
+++ client/templates/gameStats/stats/replacement.js
@@ -3,11 +3,39 @@ Template.replacement.events({
 		$('#homeTeamInPlay .homeTeamPillForModal').each(function() {
 			console.log(this);
 		});
-	}
+	},
+
 });
 
 Template.replacement.onRendered(function() {
 	//dragula(this.find('#homeTeamOnTheBench'));
-	dragula([this.find('#homeTeamInPlay'), this.find('#homeTeamOnTheBench')]);
-	dragula([this.find('#awayTeamInPlay'), this.find('#awayTeamOnTheBench')]);
+	dragula([this.find('#homeTeamInPlay'), this.find('#homeTeamOnTheBench')]).on('drop', function(el, container, source) {
+		var gameId = {
+			_id: Session.get('gameId')
+		};
+		var playerData = {
+			playerIndex: Number(el.firstElementChild.firstElementChild.firstChild.data),
+			team: 'homeTeam'
+		};
+		Meteor.call('goingInPlay', gameId, playerData, function(error, result) {
+			if (error) {
+				return throwError(error.message);
+			}
+		});
+	});
+	dragula([this.find('#awayTeamInPlay'), this.find('#awayTeamOnTheBench')]).on('drop', function(el, container, source) {
+		var gameId = {
+			_id: Session.get('gameId')
+		};
+		var playerData = {
+			playerIndex: Number(el.firstElementChild.firstElementChild.firstChild.data),
+			team: 'awayTeam'
+		};
+		Meteor.call('goingInPlay', gameId, playerData, function(error, result) {
+			if (error) {
+				return throwError(error.message);
+			}
+		});
+	});
+	Session.set('gameId', Template.currentData()._id);
 });
