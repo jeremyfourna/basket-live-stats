d6f457fa80206726494777a0b251875854d029c9
dexterneo <jeremyfourna@gmail.com>
2015-06-03 13:10:18 +0200

    Add on field management feature

--- client/templates/gameStats/stats/replacement.js
+++ client/templates/gameStats/stats/replacement.js
@@ -1,12 +1,3 @@
-Template.replacement.events({
-	'click .validateInPlayPlayers': function() {
-		$('#homeTeamInPlay .homeTeamPillForModal').each(function() {
-			console.log(this);
-		});
-	},
-
-});
-
 Template.replacement.onRendered(function() {
 	//dragula(this.find('#homeTeamOnTheBench'));
 	dragula([this.find('#homeTeamInPlay'), this.find('#homeTeamOnTheBench')]).on('drop', function(el, container, source) {
@@ -17,13 +8,22 @@ Template.replacement.onRendered(function() {
 			playerIndex: Number(el.firstElementChild.firstElementChild.firstChild.data),
 			team: 'homeTeam'
 		};
-		Meteor.call('goingInPlay', gameId, playerData, function(error, result) {
-			if (error) {
-				return throwError(error.message);
-			}
-		});
+		if (container.id === 'homeTeamInPlay') {
+			Meteor.call('goingInPlay', gameId, playerData, function(error, result) {
+				if (error) {
+					return throwError(error.message);
+				}
+			});
+		} else if (container.id === 'homeTeamOnTheBench') {
+			Meteor.call('goingOnTheBench', gameId, playerData, function(error, result) {
+				if (error) {
+					return throwError(error.message);
+				}
+			});
+		}
 	});
 	dragula([this.find('#awayTeamInPlay'), this.find('#awayTeamOnTheBench')]).on('drop', function(el, container, source) {
+		console.log(container.id);
 		var gameId = {
 			_id: Session.get('gameId')
 		};
@@ -31,11 +31,19 @@ Template.replacement.onRendered(function() {
 			playerIndex: Number(el.firstElementChild.firstElementChild.firstChild.data),
 			team: 'awayTeam'
 		};
-		Meteor.call('goingInPlay', gameId, playerData, function(error, result) {
-			if (error) {
-				return throwError(error.message);
-			}
-		});
+		if (container.id === 'awayTeamInPlay') {
+			Meteor.call('goingInPlay', gameId, playerData, function(error, result) {
+				if (error) {
+					return throwError(error.message);
+				}
+			});
+		} else if (container.id === 'awayTeamOnTheBench') {
+			Meteor.call('goingOnTheBench', gameId, playerData, function(error, result) {
+				if (error) {
+					return throwError(error.message);
+				}
+			});
+		}
 	});
 	Session.set('gameId', Template.currentData()._id);
 });
