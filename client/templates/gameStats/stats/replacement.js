bc7d39ba064c8d242452e364a4898763b11eecc6
dexterneo <jeremyfourna@gmail.com>
2015-06-03 13:54:08 +0200

    Display player in play and on the bench on replacement page and stats page

--- client/templates/gameStats/stats/replacement.js
+++ client/templates/gameStats/stats/replacement.js
@@ -1,5 +1,4 @@
 Template.replacement.onRendered(function() {
-	//dragula(this.find('#homeTeamOnTheBench'));
 	dragula([this.find('#homeTeamInPlay'), this.find('#homeTeamOnTheBench')]).on('drop', function(el, container, source) {
 		var gameId = {
 			_id: Session.get('gameId')
@@ -23,7 +22,6 @@ Template.replacement.onRendered(function() {
 		}
 	});
 	dragula([this.find('#awayTeamInPlay'), this.find('#awayTeamOnTheBench')]).on('drop', function(el, container, source) {
-		console.log(container.id);
 		var gameId = {
 			_id: Session.get('gameId')
 		};
@@ -46,4 +44,47 @@ Template.replacement.onRendered(function() {
 		}
 	});
 	Session.set('gameId', Template.currentData()._id);
+});
+
+Template.replacement.helpers({
+	'homeTeamPlayersPlaying': function() {
+		var team = this.homeTeam.players;
+		var inPlayTeam = [];
+		team.forEach(function(element, index, array) {
+			if (element.inPlay) {
+				inPlayTeam.push(element);
+			}
+		});
+		return inPlayTeam;
+	},
+	'homeTeamPlayersOnTheBench': function() {
+		var team = this.homeTeam.players;
+		var oneTheBenchTeam = [];
+		team.forEach(function(element, index, array) {
+			if (!element.inPlay) {
+				oneTheBenchTeam.push(element);
+			}
+		});
+		return oneTheBenchTeam;
+	},
+	'awayTeamPlayersPlaying': function() {
+		var team = this.awayTeam.players;
+		var inPlayTeam = [];
+		team.forEach(function(element, index, array) {
+			if (element.inPlay) {
+				inPlayTeam.push(element);
+			}
+		});
+		return inPlayTeam;
+	},
+	'awayTeamPlayersOnTheBench': function() {
+		var team = this.awayTeam.players;
+		var oneTheBenchTeam = [];
+		team.forEach(function(element, index, array) {
+			if (!element.inPlay) {
+				oneTheBenchTeam.push(element);
+			}
+		});
+		return oneTheBenchTeam;
+	}
 });
