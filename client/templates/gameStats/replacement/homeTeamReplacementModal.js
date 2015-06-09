2b494f14abfc9aba96e7d252187c99a36a1e8390
dexterneo <jeremyfourna@gmail.com>
2015-06-09 13:21:14 +0200

    Call the method for the player switch

---
+++ client/templates/gameStats/replacement/homeTeamReplacementModal.js
@@ -0,0 +1,29 @@
+Template.homeTeamReplacementModal.events({
+	'click #validateSwitch': function() {
+		var data = Session.get('switchData');
+		var gameId = {
+			_id: data._id
+		};
+		var playerData = {
+			team: data.team,
+			playerIndex: data.playerIndex,
+			minutes: Number($('#homeTeamMinutes').val()),
+			secondes: Number($('#homeTeamSecondes').val())
+		};
+		if (data.call === 'goingInPlay') {
+			Meteor.call('goingInPlay', gameId, playerData, function(error, result) {
+				if (error) {
+					return throwError(error.message);
+				}
+			});
+		} else if (data.call === 'goingOnTheBench') {
+			Meteor.call('goingOnTheBench', gameId, playerData, function(error, result) {
+				if (error) {
+					return throwError(error.message);
+				}
+			});
+		}
+
+
+	}
+});
