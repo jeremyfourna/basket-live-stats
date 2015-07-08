8e0d67edfd566408f9776f5a884ebc770be0b02b
dexterneo <jeremyfourna@gmail.com>
2015-07-08 13:08:18 +0200

    Create graph template

---
+++ client/templates/gameStats/stats/graph.js
@@ -0,0 +1,69 @@
+Template.graph.onRendered(function() {
+	var data = new ReactiveVar(Template.currentData().gameStats.evolution);
+	var dataset = data.get();
+
+	var margin = {
+		top: 20,
+		right: 20,
+		bottom: 20,
+		left: 30
+	};
+	var width = $('.container').width() - 60;
+	var height = 200 - margin.top - margin.bottom;
+
+	var x = d3.scale.linear()
+		.range([0, width]);
+
+	var y = d3.scale.linear()
+		.range([height, 0]);
+
+	var xAxis = d3.svg.axis()
+		.scale(x)
+		.orient("bottom");
+
+	var yAxis = d3.svg.axis()
+		.scale(y)
+		.orient("left")
+		.ticks(5);
+
+	var line = d3.svg.line()
+		.x(function(d) {
+			return x(d[0]);
+		})
+		.y(function(d) {
+			return y(d[1]);
+		});
+
+	var svg = d3.select("#scoreChart").append("svg")
+		.attr("width", width)
+		.attr("height", height + margin.top + margin.bottom)
+		.append("g")
+		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+	x.domain(d3.extent(dataset, function(d) {
+		return d[0];
+	}));
+	y.domain(d3.extent(dataset, function(d) {
+		return d[1];
+	}));
+
+	/*svg.append("g")
+		.attr("class", "x axis")
+		.attr("transform", "translate(0," + height + ")")
+		.call(xAxis);*/
+
+	svg.append("g")
+		.attr("class", "y axis")
+		.call(yAxis)
+		.append("text")
+		.attr("transform", "rotate(-90)")
+		.attr("y", 6)
+		.attr("dy", ".71em")
+		.style("text-anchor", "end")
+		.text("Ecart du match");
+
+	svg.append("path")
+		.datum(dataset)
+		.attr("class", "line")
+		.attr("d", line);
+});
