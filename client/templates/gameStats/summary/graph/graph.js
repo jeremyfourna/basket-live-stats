0c5d9eea20c2f334b75b3106f913c5f16c77bdee
dexterneo <jeremyfourna@gmail.com>
2015-07-19 00:20:26 +0200

    Add a graph via c3.js but it doesn't update in realtime

--- client/templates/gameStats/summary/graph/graph.js
+++ client/templates/gameStats/summary/graph/graph.js
@@ -1,96 +1,43 @@
-/*
-Template.graph.onRendered(function() {
-	var data = new ReactiveVar(Template.currentData().gameStats.evolution);
-	var dataset = data.get();
-
-	var margin = {
-		top: 20,
-		right: 20,
-		bottom: 20,
-		left: 30
-	};
-	var width = $('.container').width() - 60;
-	var height = 200 - margin.top - margin.bottom;
-
-	var x = d3.scale.linear()
-		.range([0, width]);
-
-	var y = d3.scale.linear()
-		.range([height, 0]);
-
-	var xAxis = d3.svg.axis()
-		.scale(x)
-		.orient("bottom");
-
-	var yAxis = d3.svg.axis()
-		.scale(y)
-		.orient("left")
-		.ticks(5);
-
-	var line = d3.svg.line()
-		.x(function(d) {
-			return x(d[0]);
-		})
-		.y(function(d) {
-			return y(d[1]);
-		});
-
-	var svg = d3.select("#scoreChart").append("svg")
-		.attr("width", width)
-		.attr("height", height + margin.top + margin.bottom)
-		.append("g")
-		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-	x.domain(d3.extent(dataset, function(d) {
-		return d[0];
-	}));
-	y.domain(d3.extent(dataset, function(d) {
-		return d[1];
-	}));
-
-	svg.append("g")
-		.attr("class", "y axis")
-		.call(yAxis)
-		.append("text")
-		.attr("transform", "rotate(-90)")
-		.attr("y", 6)
-		.attr("dy", ".71em")
-		.style("text-anchor", "end")
-		.text("Ecart du match");
-
-	svg.append("path")
-		.datum(dataset)
-		.attr("class", "line")
-		.attr("d", line);
-});
-*/
-
 Template.graph.helpers({
 	graphWidth: function() {
 		return $('.container').width();
 	}
 });
 
-Template.graph.topGenresChart = function() {
-	return {
-		chart: {
-			plotBackgroundColor: null,
-			plotBorderWidth: null,
-			plotShadow: false
+Template.graph.rendered = function() {
+	var gameData = Template.currentData().gameStats.evolution;
+	var xArray = ['x'];
+	var yArray = ['data1'];
+	for (var i = 0; i < gameData.length; i++) {
+		xArray.push(gameData[i][0]);
+		yArray.push(gameData[i][1]);
+	}
+
+	Session.set('x', xArray);
+	Session.set('data1', yArray);
+	var chart = c3.generate({
+		bindto: this.find('#gameGapChart'),
+		data: {
+			xs: {
+				'data1': 'x',
+			},
+			columns: [
+				['data1'],
+			]
 		},
-		title: {
-			text: "Ecart du match"
-		},
-		plotOptions: {
-			line: {
-				allowPointSelect: false,
-				cursor: 'pointer',
+		axis: {
+			x: {
+				show: false
 			}
-		},
-		series: [{
-			type: 'line',
-			name: 'Ecart du match',
-			data: Template.currentData().gameStats.evolution
-		}]
-	};
-};
+		}
+	});
+
+	this.autorun(function(tracker) {
+		chart.load({
+			columns: [
+				Session.get('x'),
+				Session.get('data1'), []
+			]
+		});
+	});
+}
