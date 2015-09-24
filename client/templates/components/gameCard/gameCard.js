047ae29f7d3b744095cd228f2a59f58b87c4de8e
dexterneo <jeremyfourna@gmail.com>
2015-09-24 13:40:34 +0200

    Finish gameCard component

--- client/templates/components/gameCard/gameCard.js
+++ client/templates/components/gameCard/gameCard.js
@@ -3,14 +3,97 @@ var GameCard = BlazeComponent.extendComponent({
 		return 'gameCard';
 	},
 	gameData: function() {
-		var game = Games.findOne({
-			'_id': this.data()._id
-		});
-		console.log(game);
-		return game;
+		return this.data();
 	},
-	gameState: function() {
-		var game = Games.findOne(this.data().id);
-		return '{{_ ' + game.gameState + '}}';
+	q1Running: function() {
+		if (this.data().gameState === 'q1Running') {
+			return true;
+		} else {
+			return false;
+		}
+	},
+	q1Ended: function() {
+		if (this.data().gameState === 'q1Ended') {
+			return true;
+		} else {
+			return false;
+		}
+	},
+	q2Running: function() {
+		if (this.data().gameState === 'q2Running') {
+			return true;
+		} else {
+			return false;
+		}
+	},
+	halfTime: function() {
+		if (this.data().gameState === 'halfTime') {
+			return true;
+		} else {
+			return false;
+		}
+	},
+	q3Running: function() {
+		if (this.data().gameState === 'q3Running') {
+			return true;
+		} else {
+			return false;
+		}
+	},
+	q3Ended: function() {
+		if (this.data().gameState === 'q3Ended') {
+			return true;
+		} else {
+			return false;
+		}
+	},
+	q4Running: function() {
+		if (this.data().gameState === 'q4Running') {
+			return true;
+		} else {
+			return false;
+		}
+	},
+	gameEnded: function() {
+		if (this.data().gameState === 'gameEnded') {
+			return true;
+		} else {
+			return false;
+		}
+	},
+	oT1: function() {
+		if (this.data().gameState === 'oT1') {
+			return true;
+		} else {
+			return false;
+		}
+	},
+	oT2: function() {
+		if (this.data().gameState === 'oT2') {
+			return true;
+		} else {
+			return false;
+		}
+	},
+	oT3: function() {
+		if (this.data().gameState === 'oT3') {
+			return true;
+		} else {
+			return false;
+		}
+	},
+	oT4: function() {
+		if (this.data().gameState === 'oT4') {
+			return true;
+		} else {
+			return false;
+		}
+	},
+	oT5: function() {
+		if (this.data().gameState === 'oT5') {
+			return true;
+		} else {
+			return false;
+		}
 	}
 }).register('GameCard');
