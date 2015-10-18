4af1778196c7558deb45c7bd089005932fdb5276
dexterneo <jeremyfourna@gmail.com>
2015-10-18 15:38:12 +0200

    WIP refactor playerBio feature

--- client/templates/gameStats/playersSettings/playerBio.js
+++ client/templates/gameStats/playersSettings/playerBio.js
@@ -1,5 +1,17 @@
 Template.playerBio.helpers({
 	gameData: function() {
 		return Games.findOne(Router.current().params._id);
+	},
+	playerData: function() {
+		return Players.find({
+			gameId: Router.current().params._id,
+			teamId: 'yourClub'
+		});
+	},
+	coachData: function() {
+		return Coachs.find({
+			gameId: Router.current().params._id,
+			teamId: 'yourClub'
+		});
 	}
 });
