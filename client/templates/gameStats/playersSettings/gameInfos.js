3ebce31d77037a89b9dc9794b7bb77d13a1a9804
dexterneo <jeremyfourna@gmail.com>
2015-10-18 22:22:08 +0200

    Refactor playerSettings feature

--- client/templates/gameStats/playersSettings/gameInfos.js
+++ client/templates/gameStats/playersSettings/gameInfos.js
@@ -1,11 +1,4 @@
 Template.gameInfos.events({
-	'click .gameInfosUpdate': function() {
-		$('.teamHome').removeAttr('disabled');
-		$('.teamAway').removeAttr('disabled');
-		$('.level').removeAttr('disabled');
-		$('.group').removeAttr('disabled');
-		$('.gameInfosValidation').removeClass('disabled');
-	},
 	'click .gameInfosValidation': function() {
 		var gameInfos = {
 			yourClub: $('.teamHome').val(),
@@ -16,12 +9,6 @@ Template.gameInfos.events({
 		Meteor.call('gameInfosUpdate', Router.current().params._id, gameInfos, function(error) {
 			if (error) {
 				return throwError(error.message);
-			} else {
-				$('.teamHome').attr('disabled', 'disabled');
-				$('.teamAway').attr('disabled', 'disabled');
-				$('.level').attr('disabled', 'disabled');
-				$('.group').attr('disabled', 'disabled');
-				$('.gameInfosValidation').addClass('disabled');
 			}
 		});
 	}
--- client/templates/gameStats/playersSettings/playerUpdate.js
+++ client/templates/gameStats/playersSettings/playerUpdate.js
@@ -1,34 +1,11 @@
-Template.playerUpdate.helpers({
-	'modifyButton': function() {
-		return this.team + 'update' + this.jersey;
-	},
-	'validationButton': function() {
-		return this.team + 'validation' + this.jersey;
-	}
-});
-
 Template.playerUpdate.events({
-	'click .playerUpdate': function() {
-		var updateSelector = '#' + this.team + 'update' + this.jersey;
-		var validateSelector = '#' + this.team + 'validation' + this.jersey;
-		$(updateSelector).closest('.player').find('.firstName').removeAttr('disabled');
-		$(updateSelector).closest('.player').find('.lastName').removeAttr('disabled');
-		$(updateSelector).closest('.player').find(validateSelector).removeClass('disabled');
-	},
 	'click .playerValidation': function(e) {
-		var updateSelector = '#' + this.team + 'update' + this.jersey;
-		var validateSelector = '#' + this.team + 'validation' + this.jersey;
-		var gameId = Template.parentData(1)._id;
+		e.preventDefault();
 		var playerInfo = {
-			firstName: $(validateSelector).closest('.player').find('.firstName').val(),
-			lastName: $(validateSelector).closest('.player').find('.lastName').val(),
-			team: this.team,
-			playerIndex: this.playerIndex,
-			gameId: gameId
+			lastName: e.target.parentElement[0].value,
+			firstName: e.target.parentElement[1].value,
+			playerId: this._id
 		};
-		$(validateSelector).closest('.player').find('.firstName').attr('disabled', 'disabled');
-		$(validateSelector).closest('.player').find('.lastName').attr('disabled', 'disabled');
-		$(validateSelector).addClass('disabled');
 		Meteor.call('playerUpdate', playerInfo, function(error) {
 			if (error) {
 				return throwError(error.message);
