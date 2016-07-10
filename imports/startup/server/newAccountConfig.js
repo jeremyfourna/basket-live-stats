d94603e73196755840b4adbc1a7c94743a3b56b2
dexterneo <jeremyfourna@gmail.com>
2016-07-10 17:07:51 +0200

    Fix eslint error

--- imports/startup/server/newAccountConfig.js
+++ imports/startup/server/newAccountConfig.js
@@ -1,9 +1,9 @@
 import { Accounts } from 'meteor/accounts-base';
 
 Accounts.onCreateUser(function(options, user) {
-	options.profile.name = "";
-	options.profile.firstName = "";
-	options.profile.club = "";
+	options.profile.name = '';
+	options.profile.firstName = '';
+	options.profile.club = '';
 	options.profile.basicUser = true;
 	options.profile.proUser = false;
 	options.profile.clubAdmin = false;
