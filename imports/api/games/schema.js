ba62a93e47bf14f241699f574721334940d5186e
jeremyfourna <jeremyfourna@gmail.com>
2016-11-25 20:22:36 +0100

    Fix eslint issues

--- imports/api/games/schema.js
+++ imports/api/games/schema.js
@@ -4,75 +4,75 @@ import { SimpleSchema } from 'meteor/aldeed:simple-schema';
 export const Games = new Mongo.Collection('games');
 
 Games.deny({
-    insert() {
-        return true;
-    },
-    update() {
-        return true;
-    },
-    remove() {
-        return true;
-    }
+	insert() {
+		return true;
+	},
+	update() {
+		return true;
+	},
+	remove() {
+		return true;
+	}
 });
 
 export const gameSchema = new SimpleSchema({
-    userId: {
-        type: String,
-        label: 'User id'
-    },
-    createdAt: {
-        type: String,
-        label: 'State of the game'
-    },
-    gameState: {
-        type: String,
-        label: 'State of the game',
-        allowedValues: ['notStarted', 'q1Running', 'q1Ended', 'q2Running', 'halfTime', 'q3Running', 'q3Ended', 'q4Running', 'gameEnded', 'oT1', 'oT2', 'oT3', 'oT4', 'oT5']
-    },
-    privateGame: {
-        type: Boolean,
-        label: 'Game privacy'
-    },
-    gameInfos: {
-        type: GameInfosSchema,
-        label: 'Informations about the teams playing'
-    },
-    evolution: {
-        type: [
-            [Number, Number]
-        ],
-        label: 'Game score evolution'
-    },
-    yourClub: {
-        type: String,
-        label: 'Your club name',
-        optional: true
-    },
-    yourClubTeamId: {
-        type: String,
-        label: 'Your club team id in the Teams collection',
-        optional: true
-    },
-    opponent: {
-        type: String,
-        label: 'Opponent club name',
-        optional: true
-    },
-    opponentTeamId: {
-        type: String,
-        label: 'Your club team id in the Teams collection',
-        optional: true
-    },
-    level: {
-        type: String,
-        label: 'Game level',
-        optional: true
-    },
-    group: {
-        type: String,
-        label: 'Group of the both teams',
-        optional: true
-    }
+	userId: {
+		type: String,
+		label: 'User id'
+	},
+	createdAt: {
+		type: String,
+		label: 'State of the game'
+	},
+	gameState: {
+		type: String,
+		label: 'State of the game',
+		allowedValues: ['notStarted', 'q1Running', 'q1Ended', 'q2Running', 'halfTime', 'q3Running', 'q3Ended', 'q4Running', 'gameEnded', 'oT1', 'oT2', 'oT3', 'oT4', 'oT5']
+	},
+	privateGame: {
+		type: Boolean,
+		label: 'Game privacy'
+	},
+	gameInfos: {
+		type: GameInfosSchema,
+		label: 'Informations about the teams playing'
+	},
+	evolution: {
+		type: [
+			[Number, Number]
+		],
+		label: 'Game score evolution'
+	},
+	yourClub: {
+		type: String,
+		label: 'Your club name',
+		optional: true
+	},
+	yourClubTeamId: {
+		type: String,
+		label: 'Your club team id in the Teams collection',
+		optional: true
+	},
+	opponent: {
+		type: String,
+		label: 'Opponent club name',
+		optional: true
+	},
+	opponentTeamId: {
+		type: String,
+		label: 'Your club team id in the Teams collection',
+		optional: true
+	},
+	level: {
+		type: String,
+		label: 'Game level',
+		optional: true
+	},
+	group: {
+		type: String,
+		label: 'Group of the both teams',
+		optional: true
+	}
 });
 
 Games.schema = gameSchema;
