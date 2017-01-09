636ec7b9bf708c2aafb42128b021b61370805e1a
Jeremy Fourna <jeremyfourna@gmail.com>
2017-01-09 16:36:58 +0100

    Change publications name

--- imports/api/federationConfig/server/publications.js
+++ imports/api/federationConfig/server/publications.js
@@ -2,17 +2,15 @@ import { Meteor } from 'meteor/meteor';
 
 import { FederationConfig } from '../schema.js';
 
-// Publication who send back everything, use it carrefully
-Meteor.publish('federationConfig', () => {
+Meteor.publish('retrieveAllFederationConfig', () => {
 	return FederationConfig.find();
 });
 
-// Publication who send back only the level of the categories
-Meteor.publish('championshipLevel', () => {
+Meteor.publish('retrieveAllLevel', () => {
 	return FederationConfig.find({ def: 'level' });
 });
 
 // Publication who send back only the group of categories
-Meteor.publish('championshipGroup', () => {
+Meteor.publish('retrieveAllGroup', () => {
 	return FederationConfig.find({ def: 'group' });
 });
