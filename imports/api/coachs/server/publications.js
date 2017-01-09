d4854f9a4bd8f31015aa4b7c6982bb1ce9ff4dd1
Jeremy Fourna <jeremyfourna@gmail.com>
2017-01-09 16:36:17 +0100

    Improve readability

--- imports/api/coachs/server/publications.js
+++ imports/api/coachs/server/publications.js
@@ -3,7 +3,5 @@ import { Meteor } from 'meteor/meteor';
 import { Coachs } from '../schema.js';
 
 Meteor.publish('coachsForAGame', (gameId) => {
-	return Coachs.find({
-		gameId
-	});
+	return Coachs.find({ gameId });
 });
