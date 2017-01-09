48f62f23ca56b58f430b968132e716e5c95e0860
Jeremy Fourna <jeremyfourna@gmail.com>
2017-01-09 16:35:08 +0100

    Refactore code

--- imports/startup/server/newAccountConfig.js
+++ imports/startup/server/newAccountConfig.js
@@ -1,15 +1,9 @@
 import { Accounts } from 'meteor/accounts-base';
 
 Accounts.onCreateUser((options, user) => {
-	options.profile.name = '';
-	options.profile.firstName = '';
-	options.profile.club = '';
-	options.profile.basicUser = true;
-	options.profile.proUser = false;
-	options.profile.clubAdmin = false;
-	options.profile.LBBSAdmin = false;
-	if (options.profile) {
-		user.profile = options.profile;
-	}
+	user.profile = options.profile;
+	user.profile.name = '';
+	user.profile.firstName = '';
+	user.profile.status = 'active';
 	return user;
 });
