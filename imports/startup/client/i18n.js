1d7aa0a9cc889e3fdd325acfebb5e9cd0c0af725
jeremyfourna <jeremyfourna@gmail.com>
2017-08-13 17:40:52 +0200

    Add translation

--- imports/startup/client/i18n.js
+++ imports/startup/client/i18n.js
@@ -2,15 +2,15 @@ import { Meteor } from 'meteor/meteor';
 import { TAPi18n } from 'meteor/tap:i18n';
 import { Bert } from 'meteor/themeteorchef:bert';
 
-let getUserLanguage = () => {
+function getUserLanguage() {
 	// Put here the logic for determining the user language
 	return 'fr';
-};
+}
 
 if (Meteor.isClient) {
 	Meteor.startup(() => {
-		TAPi18n.setLanguage(getUserLanguage()).fail((error_message) => {
-			return Bert.alert(error_message, 'danger', 'growl-top-right');
+		TAPi18n.setLanguage(getUserLanguage()).fail((errorMessage) => {
+			return Bert.alert(errorMessage, 'danger', 'growl-top-right');
 		});
 	});
 }
