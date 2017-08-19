e3c2b4055a7a091875e8c869971c63d553c59016
jeremyfourna <jeremyfourna@gmail.com>
2017-08-19 15:49:35 +0200

    Add i18n behaviour

--- imports/startup/client/i18n.js
+++ imports/startup/client/i18n.js
@@ -1,10 +1,15 @@
 import { Meteor } from 'meteor/meteor';
 import { TAPi18n } from 'meteor/tap:i18n';
 import { Bert } from 'meteor/themeteorchef:bert';
+import R from 'ramda';
 
 function getUserLanguage() {
 	// Put here the logic for determining the user language
-	return 'fr';
+	if (R.isNil(R.path(['profile', 'preferredLanguage'], Meteor.user()))) {
+		return 'en';
+	} else {
+		return R.path(['profile', 'preferredLanguage'], Meteor.user());
+	}
 }
 
 if (Meteor.isClient) {
